4f0bec2d03ba2b2d0edb67072aa39497b33219c1
Mikhail Maslyuk <leakymee@bu.edu>
2018-10-19 20:56:16 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const uuidv4 = require('uuid/v4');
+
+const app = express();
+
+const products = ['Shoes', 'Cars', 'Jewelery', "Men's Clothing", "Women's Clothing", 'Education', 'Consulting']
+
+const randomProductName = () => {
+	return products[Math.floor(Math.random() * (products.length - 0)) + 0]
+}
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use('/', (req, res) => {
+	console.log('Checking if user has sent us a cookie.')
+	console.log('Logging Request Headers.')
+	console.log(req.headers)
+	if (!req.headers['Cookie']) {
+		console.log('Processed Request - User Does Not Have Cookie.')
+		const uniqueID = uuid4();
+		res.setHeader('Set-Cookie', [`id=${uniqueID}`, `contentFocus=${randomProductName()}`]);
+	}
+	console.log("Logging Response Headers.")
+	console.log(res.headers)
+	res.send("Response Set")
+})
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port)
+
+console.log(`Audience Service Host listening on ${port}`)
