39dbe7d5347d2db69174e21120409b5dc7908dc0
Mikhail Maslyuk <leakymee@bu.edu>
2018-10-22 20:27:11 -0400

    Maintain local cookies for client preferences for ad retarget

--- client/src/App.js
+++ client/src/App.js
@@ -5,40 +5,61 @@ import './App.css';
 
 
 class App extends Component {
-  state = {
-    interestArray: ['Cooking', 'Luxury', 'Style', 'Weddings'],
-    activeInterest: null,
-  }
+    state = {
+     	interestArray: ['Cooking', 'Cars', 'Style', 'Weddings'],
+     	activeInterest: null,
+    }
 
   componentDidMount() {
-    this.setState({
-      activeInterest: this.findActiveInterestFromCookies()
-    })
+  	this.setState({
+  		activeInterest: this.findActiveInterestFromCookies()
+  	})
   }
 
   findActiveInterestFromCookies = () => {
     const cookies = document.cookie;
     console.log(cookieParser.getItem('contentFocus'))
     const interest = cookieParser.getItem('contentFocus')
+    console.log(cookieParser.getItem('unreal'))
       return interest
   }
 
+  generateInterestColumn = () => {
+    if (this.state.activeInterest === null) {
+      return "Loading..."
+    } else if (this.state.activeInterest === undefined) {
+      return "An interest profile has not been generated for this client yet"
+    } else if (this.state.activeInterest) {
+      return `This Client is interested in ${this.state.activeInterest}` 
+    }
+  }
+  
+  updateClientPreferences = (interest) => {
+    cookieParser.setItem('contentFocus', interest)
+    this.setState({
+      activeInterest: interest
+    })
+  }
+
+  renderInteractionMenu = () => {
+    return this.state.interestArray.map(interest => {
+    	return (
+	      <div onClick={() => this.updateClientPreferences(interest)} className={`interest-window${(this.state.activeInterest === interest) ? '' : ' active-interest'}`}>
+	      	<img className='interest-image' src={`/${interest}.jpg`} />
+	      </div>
+      	)
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            This is the Audience Service Provider
+            {this.generateInterestColumn()}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className='responsive-flex'>
+            {this.renderInteractionMenu()}
+          </div>
         </header>
       </div>
     );
